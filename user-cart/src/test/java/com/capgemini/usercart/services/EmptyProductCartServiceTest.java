9f09582595db81b9969f78595fcfd99c2ada3e58
srinu1794 <sdwarak8877@gmail.com>
2018-02-12 16:11:27 +0530

    adding test case

--- user-cart/src/test/java/com/capgemini/usercart/services/EmptyProductCartServiceTest.java
+++ user-cart/src/test/java/com/capgemini/usercart/services/EmptyProductCartServiceTest.java
@@ -1,91 +1,58 @@
 package com.capgemini.usercart.services;
 
-import org.junit.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
 import com.capgemini.usercart.repository.MongoRepositoryDataBaseOperations;
 
-/**
- * The class <code>EmptyProductCartServiceTest</code> contains tests for the class <code>{@link EmptyProductCartService}</code>.
- *
- * @generatedBy CodePro at 1/30/18 7:13 PM
- * @author dsriniv1
- * @version $Revision: 1.0 $
- */
 public class EmptyProductCartServiceTest {
-	/**
-	 * Run the String emptyCart(String) method test.
-	 *
-	 * @throws Exception
-	 *
-	 * @generatedBy CodePro at 1/30/18 7:13 PM
-	 */
+	
+	
+	@InjectMocks
+	EmptyProductCartService emptyProductCartService;
+	
+	@Mock
+	MongoRepositoryDataBaseOperations mongoRepositoryDataBaseOperations;
+	
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+
+	
 	@Test
 	public void testEmptyCart_1()
 		throws Exception {
-		EmptyProductCartService fixture = new EmptyProductCartService();
-		fixture.mongoRepositoryDataBaseOperations = null;
-		String userId = "";
+		
+		Mockito.when(mongoRepositoryDataBaseOperations.exists(Mockito.isA(String.class))).thenReturn(true);
+		
+		String emptyCart = emptyProductCartService.emptyCart("xyz");
 
-		String result = fixture.emptyCart(userId);
-
-		// add additional test code here
-		assertEquals("Failed", result);
+	
+		assertEquals("Success", emptyCart);
 	}
-
-	/**
-	 * Run the String emptyCart(String) method test.
-	 *
-	 * @throws Exception
-	 *
-	 * @generatedBy CodePro at 1/30/18 7:13 PM
-	 */
+	
 	@Test
 	public void testEmptyCart_2()
 		throws Exception {
-		EmptyProductCartService fixture = new EmptyProductCartService();
-		fixture.mongoRepositoryDataBaseOperations = null;
-		String userId = "";
+		
+		Mockito.when(mongoRepositoryDataBaseOperations.exists(Mockito.isA(String.class))).thenReturn(false);
+		Mockito.when(mongoRepositoryDataBaseOperations.exists(Mockito.isA(String.class))).thenThrow(Exception.class);
 
-		String result = fixture.emptyCart(userId);
+		String emptyCart = emptyProductCartService.emptyCart("xyz");
 
-		// add additional test code here
-		assertEquals("Failed", result);
+	
+		assertEquals("Failed", emptyCart);
 	}
-
-	/**
-	 * Perform pre-test initialization.
-	 *
-	 * @throws Exception
-	 *         if the initialization fails for some reason
-	 *
-	 * @generatedBy CodePro at 1/30/18 7:13 PM
-	 */
-	@Before
-	public void setUp()
-		throws Exception {
-		// add additional set up code here
-	}
-
-	/**
-	 * Perform post-test clean-up.
-	 *
-	 * @throws Exception
-	 *         if the clean-up fails for some reason
-	 *
-	 * @generatedBy CodePro at 1/30/18 7:13 PM
-	 */
-	@After
-	public void tearDown()
-		throws Exception {
-		// Add additional tear down code here
-	}
-
-	/**
-	 * Launch the test.
-	 *
-	 * @param args the command line arguments
-	 *
-	 * @generatedBy CodePro at 1/30/18 7:13 PM
-	 */
+	
+	
+	
 	
 }
