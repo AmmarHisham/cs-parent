6fb8709779b5b23f67b4a1304bed7d3caa494f73
ppallama <ppallama@DIN37001028.corp.capgemini.com>
2018-02-13 11:28:04 +0530

    updated

--- feedback-rating/src/main/java/com/cg/service/CustomerService.java
+++ feedback-rating/src/main/java/com/cg/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.cg.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.cg.entity.CustomerFeedback;
